0b5138624833f4d8fbeec73a2958e81619f9745b
Mdkhaki <mdkhaki@yahoo.com>
2021-04-13 09:36:31 +0430

    Initial

---
+++ src/test/java/com/mdkhaki/springboot_registration_and_login/SpringbootRegistrationAndLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdkhaki.springboot_registration_and_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRegistrationAndLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
