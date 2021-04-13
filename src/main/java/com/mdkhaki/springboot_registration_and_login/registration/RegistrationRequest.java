73a6453507b89d8432e121db84744a9cb44397fc
Mdkhaki <mdkhaki@yahoo.com>
2021-04-13 19:04:40 +0430

    security config done!

---
+++ src/main/java/com/mdkhaki/springboot_registration_and_login/registration/RegistrationRequest.java
@@ -0,0 +1,14 @@
+package com.mdkhaki.springboot_registration_and_login.registration;
+
+import lombok.*;
+
+@Getter
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class RegistrationRequest {
+    private final String firstName;
+    private final String lastName;
+    private final String email;
+    private final String password;
+}
